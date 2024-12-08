f029dbda7beb78bd750b7a4eb91aa01c8b4be60d
emonmahdi <developeremon1997@gmail.com>
2024-12-08 20:37:33 +0600

    setup tailwind css and daisy UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-      <h2>Career Hub</h2>
+      <h2 className="bg-blue-400 text-3xl p-4">Career Hub</h2>
+      <button className="btn btn-primary">Primary</button>
     </>
   );
 }
